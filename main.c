447a53e2f73c3f94df09a7a6603612743d2d601a
gerda-ugne <gerda@LAPTOP-B21F1DFU.localdomain>
2020-11-14 13:39:40 +0000

    Added testing prototype prompts

--- main.c
+++ main.c
@@ -11,14 +11,14 @@ int main()
     Cell **cells = initializeArray(10);
     if(cells != NULL) printf ("Array intialized successfully!\n");
 
-    if (fillGeneration(cells,30) == SUCCESS)
+    if (fillGeneration(cells,10) == SUCCESS)
     {
         printf("Generation filled successfully!\n");
     }
     displayGeneration(cells, 10);
     Rules *rules = generateRuleValues(30);
 
-    for(int i=0; i<50; i++)
+    for(int i=0; i<20; i++)
     {
         calculateNextGeneration(cells, rules);
         displayGeneration(cells,10);
